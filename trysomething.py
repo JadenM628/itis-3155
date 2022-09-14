5e35be6afbd1200720e4502ceb4040331c0603f6
JadenM628 <marquezgaming628@gmail.com>
2022-09-14 16:36:52 -0400

    Update trysomething.py

--- trysomething.py
+++ trysomething.py
@@ -3,7 +3,7 @@ def string_both_ends(str):
         return ''
     return str[0:2] + str[-2:]
 
-print('Enter your String: ')
+print('Enter your String:')
 string = input()
 stringToReturn = string_both_ends(string)
 print(stringToReturn)
